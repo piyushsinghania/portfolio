0bc8a328cc57ed15cd1738788a24ef129fc3a8be
Piyush Singhania <piyushsinghania28@gmail.com>
2021-09-30 20:36:59 +0530

    Initial commit

---
+++ src/components/Project.vue
@@ -0,0 +1,46 @@
+<template>
+  <div v-for="project in projects" :key="project.id">
+    <div class="project">
+      <div class="details">
+        <h3> {{project.title}} </h3>
+        <h6> {{project.role}} </h6>
+        <p> "Hey there" </p>
+        <div v-for="tech in project.stack" :key="tech">
+          <img :src="require(`../assets/${project.tech}`)" alt="">
+        </div>
+      </div>
+      <div class="picture">
+        <img :src="require(`../assets/${project.filename}`)" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["projects"]
+}
+</script>
+
+<style>
+  .project {
+    border: 2px solid grey;
+    margin: 40px auto;
+    width: 85%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  } 
+  .details {
+    border: 2px solid red;
+    width: 55%;
+  }
+  .picture {
+    border: 2px solid black;
+    width: 40%;
+  }
+  .picture img {
+    width: 450px;
+  }
+</style>
---
+++ src/views/Projects.vue
@@ -0,0 +1,56 @@
+<template>
+  <Project :projects="projects" />
+</template>
+
+<script>
+import Project from '../components/Project.vue'
+export default {
+  components: {
+    Project
+  },
+  data() {
+    return {
+      projects: [
+        {
+          id: 1,
+          title: "Datoms Inverter Dashboard",
+          role: "Full stack developer",
+          tags: ["Internship"],
+          filename: "Datoms.png"
+        },
+        {
+          id: 2,
+          title: "Acorn Globus",
+          role: "Frontend developer",
+          tags: ["Freelance"],
+          filename: "Acorn-Globus.png"
+        },
+        {
+          id: 3,
+          title: "Healthy Life",
+          role: "Frontend developer",
+          tags: ["Project"],
+          filename: "healthylife.png"
+        },
+        {
+          id: 4,
+          title: "Dojo Blog",
+          role: "Frontend developer",
+          tags: ["Project"],
+          filename: "Dojo-Blog.png"
+        },
+        {
+          id: 5,
+          title: "Mausam Samachar",
+          role: "Full stack developer",
+          filename: "mausam-samachar.png",
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
